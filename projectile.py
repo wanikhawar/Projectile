b89c5112fc7aedd5fbb8db0c20bf1007479b1ebe
Khawar Zamman Wani <wanikhawar@gmail.com>
2024-10-31 17:19:20 +0530

    Round projectile results to 2 decimal places

--- projectile.py
+++ projectile.py
@@ -12,18 +12,21 @@ class Projectile:
     def position(self, t):
         x = self.v0 * np.cos(self.theta) * t
         y = self.v0 * np.sin(self.theta) * t - (1 / 2) * self.g * t**2
-        return x, y
+        return np.round(x, 2), np.round(y, 2)
 
     def velocity(self, t):
         vx = self.v0 * np.cos(self.theta)
         vy = self.v0 * np.sin(self.theta) - self.g * t
-        return vx, vy
+        return np.round(vx), np.round(vy)
 
     def horizontal_range(self):
-        return (self.v0**2 * np.sin(2 * self.theta)) / self.g
+        R = (self.v0**2 * np.sin(2 * self.theta)) / self.g
+        return np.round(R, 2)
 
     def time_of_flight(self):
-        return (2 * self.v0 * np.sin(self.theta)) / self.g
+        t_f = (2 * self.v0 * np.sin(self.theta)) / self.g
+        return np.round(t_f, 2)
 
     def maximum_height(self):
-        return (self.v0 * np.sin(self.theta) ** 2) / (2 * self.g)
+        h_max = (self.v0 * np.sin(self.theta) ** 2) / (2 * self.g)
+        return np.round(h_max, 2)
